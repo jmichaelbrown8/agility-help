53023e4fac2498433dd77bd6451b9d6d9ae8a20f
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-02-18 00:43:49 -0800

    In progress commit, adding edit and create functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -74,6 +74,7 @@ router.get('/signup', (req, res) => {
     res.render('signup');
 });
 
+// dashboard page with logged in user's articles
 router.get('/dashboard', withAuth, async (req, res) => {
 
     const articlesData = await Article.findAll({
@@ -84,6 +85,37 @@ router.get('/dashboard', withAuth, async (req, res) => {
     const articles = articlesData.map(article => article.toJSON());
     res.render('dashboard', {
         articles,
+        user_id: req.session.user_id,
+        username: req.session.username,
+        logged_in: req.session.logged_in
+    });
+});
+
+// create page for a new post
+router.get('/create', withAuth, (req, res) => {
+
+    res.render('create', {
+        user_id: req.session.user_id,
+        username: req.session.username,
+        logged_in: req.session.logged_in
+    });
+});
+
+// edit page for an existing post
+router.get('/edit/:id', withAuth, async (req, res) => {
+
+    const articleData = await Article.findByPk(req.params.id);
+    const article = articleData.toJSON();
+
+    // check if this user was the author first
+    if (req.session.user_id !== article.user_id) {
+        res.render('unauthorized', {
+            logged_in: req.session.logged_in
+        });
+    }
+
+    res.render('edit', {
+        article,
         user_id: req.session.user_id,
         username: req.session.username,
         logged_in: req.session.logged_in
